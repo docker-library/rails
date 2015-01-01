7a7f05408cddabc75ca3c5252097e238021680a8
Tianon Gravi <admwiggin@gmail.com>
2014-12-31 22:27:45 -0700

    Protect against branches named "." or "onbuild" in generate-stackbrew-library.sh

--- generate-stackbrew-library.sh
+++ generate-stackbrew-library.sh
@@ -7,7 +7,7 @@ url='git://github.com/docker-library/rails'
 
 echo '# maintainer: InfoSiftr <github@infosiftr.com> (@infosiftr)'
 
-commit="$(git log -1 --format='format:%H' .)"
+commit="$(git log -1 --format='format:%H' -- .)"
 fullVersion="$(grep -m1 'ENV RAILS_VERSION ' Dockerfile | cut -d' ' -f3)"
 
 versionAliases=()
@@ -22,6 +22,8 @@ for va in "${versionAliases[@]}"; do
 	echo "$va: ${url}@${commit}"
 done
 
-echo
-commit="$(git log -1 --format='format:%H' onbuild)"
-echo "onbuild: ${url}@${commit} onbuild"
+for variant in onbuild; do
+	echo
+	commit="$(git log -1 --format='format:%H' -- "$variant")"
+	echo "$variant: ${url}@${commit} $variant"
+done
