b137a4a99d15a1bea5d796f5c729ab9f04cc6bb0
JohnMorales <jmorales@gmail.com>
2015-03-08 11:10:23 -0400

    Adding ability to run any custom script using RUN_INIT_SCRIPT environment variable

--- onbuild/entrypoint.sh
+++ onbuild/entrypoint.sh
@@ -3,5 +3,6 @@
 [ "$RUN_BUNDLE_INSTALL" == "false" ] || bundle install
 [ "$RUN_MIGRATIONS" == "true" ] && rake db:migrate
 [ "$RUN_DB_SETUP" == "true" ] && rake db:setup
+[ -n "$RUN_INIT_SCRIPT" ] && $RUN_INIT_SCRIPT
 
 exec "$@"
