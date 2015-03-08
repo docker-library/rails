9d4ba7f1556d543785bbf19614d220ba6ca99c66
JohnMorales <jmorales@gmail.com>
2015-03-08 10:05:40 -0400

    Moving bundle install to entrypoint, adding environment variable options to run/disable other commands

---
+++ onbuild/entrypoint.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+[ "$RUN_BUNDLE_INSTALL" == "false" ] || bundle install
+[ "$RUN_MIGRATIONS" == "true" ] && rake db:migrate
+[ "$RUN_DB_SETUP" == "true" ] && rake db:setup
+
+exec "$@"
