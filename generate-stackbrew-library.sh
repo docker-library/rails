73c03ee699b86b3cfbae935dd59a2767c191e7d7
Tianon Gravi <admwiggin@gmail.com>
2015-08-11 15:12:50 -0700

    Enhance "generate-stackbrew-library.sh" to only take into account changes to the Dockerfile or files from COPY in the Dockerfile for choosing the commit hash for a particular directory

--- generate-stackbrew-library.sh
+++ generate-stackbrew-library.sh
@@ -7,7 +7,7 @@ url='git://github.com/docker-library/rails'
 
 echo '# maintainer: InfoSiftr <github@infosiftr.com> (@infosiftr)'
 
-commit="$(git log -1 --format='format:%H' -- .)"
+commit="$(git log -1 --format='format:%H' -- Dockerfile $(awk 'toupper($1) == "COPY" { for (i = 2; i < NF; i++) { print $i } }' Dockerfile))"
 fullVersion="$(grep -m1 'ENV RAILS_VERSION ' Dockerfile | cut -d' ' -f3)"
 
 versionAliases=()
@@ -24,6 +24,6 @@ done
 
 for variant in onbuild; do
 	echo
-	commit="$(git log -1 --format='format:%H' -- "$variant")"
+	commit="$(cd "$variant" && git log -1 --format='format:%H' -- Dockerfile $(awk 'toupper($1) == "COPY" { for (i = 2; i < NF; i++) { print $i } }' Dockerfile))"
 	echo "$variant: ${url}@${commit} $variant"
 done
